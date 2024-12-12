fb880727592bfab0b470f97d17501f42a39e5652
That1kid333 <131219462+That1kid333@users.noreply.github.com>
2024-12-12 11:54:28 -0500

    modified package

--- vite.config.ts
+++ vite.config.ts
@@ -4,26 +4,27 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  css: {
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true,
-      },
-    },
-  },
   build: {
     outDir: 'dist',
     sourcemap: true,
+    commonjsOptions: {
+      include: [/node_modules/],
+      transformMixedEsModules: true
+    },
     rollupOptions: {
+      external: [],
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom', 'antd'],
-        },
-      },
-    },
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          antd: ['antd']
+        }
+      }
+    }
   },
-  optimizeDeps: {
-    include: ['antd'],
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
   },
   server: {
     port: 3000
