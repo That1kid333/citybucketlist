72171e5bd4306bc4c2c3f1540947692b5e772713
That1kid333 <131219462+That1kid333@users.noreply.github.com>
2024-12-12 11:49:58 -0500

    modified css

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { ConfigProvider } from 'antd';
 import App from './App.tsx';
 import './index.css';
-import 'antd/dist/reset.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ConfigProvider>
+      <App />
+    </ConfigProvider>
   </StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+      },
+    },
+  },
   build: {
     outDir: 'dist',
     sourcemap: true,
